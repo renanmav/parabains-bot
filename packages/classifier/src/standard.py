81b3373b7c6f0be8054ea08d19c1a9c145e27a06
Renan Machado <renan.mav@hotmail.com>
2020-04-02 22:56:37 -0300

    write train code for standard api

--- packages/classifier/src/standard.py
+++ packages/classifier/src/standard.py
@@ -1,6 +1,30 @@
+import nltk
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.externals import joblib
+
+nltk.download('stopwords')
+stopwords = nltk.corpus.stopwords.words('portuguese')
 
 data = pd.read_csv('./data/standard.csv', sep=';')
 data.drop('url', axis=1, inplace=True)
 
-print(data.head())
+X_train, X_test, y_train, y_test = train_test_split(data['text'], data['is_birthday'], shuffle=True)
+
+text_clf = Pipeline([
+    ('vect', CountVectorizer(stop_words=stopwords)),
+    ('tfidf', TfidfTransformer()),
+    ('clf-svm', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3))
+])
+
+text_clf = text_clf.fit(X_train, y_train)
+
+predicted = text_clf.predict(X_test)
+
+print(np.mean(predicted == y_test))
+
+joblib.dump(text_clf, 'standard.pkl')
