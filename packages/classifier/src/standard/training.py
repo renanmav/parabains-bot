f7a59052eac8e7ea1bbb0b24bc05d4db43a5e98e
Renan Machado <renan.mav@hotmail.com>
2020-04-03 17:38:33 -0300

    remove some stopwords and change alpha

--- packages/classifier/src/standard/training.py
+++ packages/classifier/src/standard/training.py
@@ -9,7 +9,7 @@ from sklearn.externals import joblib
 
 nltk.download('stopwords')
 stopwords = nltk.corpus.stopwords.words('portuguese')
-stopwords = [stopword for stopword in stopwords if stopword != 'meu']
+stopwords = list(filter(lambda w: w != 'não' and w != 'meu', stopwords))
 
 data = pd.read_csv('./data/standard.csv', sep=';')
 data.drop('url', axis=1, inplace=True)
@@ -17,10 +17,11 @@ data.drop('url', axis=1, inplace=True)
 X_train, X_test, y_train, y_test = train_test_split(
     data['text'], data['is_birthday'], shuffle=True)
 
+# Add GridSearch here to improve hyperparameter tuning
 text_clf = Pipeline([
     ('vect', CountVectorizer(stop_words=stopwords)),
     ('tfidf', TfidfTransformer()),
-    ('clf-svm', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-2))
+    ('clf-svm', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3))
 ])
 
 text_clf = text_clf.fit(X_train, y_train)
